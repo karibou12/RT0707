e8a2908e5fe3ef23c1fadf683adff2f2c575a7b8
Guillaume D <karibou12@msn.com>
2024-01-27 23:35:05 +0000

    Cleannig code

--- serveur/srv_dash.py
+++ serveur/srv_dash.py
@@ -54,6 +54,7 @@ def index():
 
 
 #*****************USER****************************
+
 #login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -105,130 +106,6 @@ def get_user(user_id):
         return jsonify({'error': 'page not found'}), 404
 
 
-# Update a specific user
-# @app.route('/update_user/<user_id>', methods=[ 'GET','POST'])
-# @login_required
-# def update_user(user_id):
-#     if request.method == 'GET':
-#         url_get = f'{urlApi}/user/{user_id}'
-#         response_get = requests.get(url_get)
-        
-#         if response_get.status_code == 200:
-#             user = response_get.json()
-#             return render_template('updateuser.html', user_id=user)
-#         else:
-#             return jsonify({'error': 'user not found'}), 404
-
-#     elif request.method == 'POST':
-#         email = request.form['email']
-#         newPassword = request.form['newPassword']
-#         confirmPassword = request.form['confirmPassword']
-#         password = request.form['password']
-
-
-#         if newPassword != confirmPassword:
-#             user= {'user':['',g.user,email]}
-#             return render_template('updateuser.html',user_id = user, message='Les password ne correspondent pas')
-#             # return render_template('updateuser.html', message='les passwords ne correpondent pas')
-
-#         elif newPassword != password:
-#             password = newPassword
-        
-#             data = {'email': email, 'password': password}
-#             url_post = f'{urlApi}/user/{user_id}'
-#             headers = {"Content-Type": "application/json"}
-#             response_post = requests.put(url_post, json=data, headers=headers)
-
-#             if response_post.status_code == 200:
-#                 return redirect(url_for('get_userfilms'))
-#             else:
-#                 return jsonify({'error': 'user update failed'}), 500
-#     return redirect(url_for('get_userfilms'))
-
-
-
-
-
-
-#***********************************FILMS*****************************************************
-
-# # Read all videos
-# @app.route('/filmAll', methods=['GET'])
-# def get_films():
-#     url = f'{urlApi}/films'
-#     response = requests.get(url)
-
-#     if response.status_code == 200:
-#         films = response.json()
-#         return render_template('films.html', films=films['films'])
-#     else:
-#         return jsonify({'error': 'page not found'}), 404
-
-
-# # Read a specific video
-# @app.route('/filmAll/<int:titre_id>', methods=['GET'])
-# def get_film(titre_id):
-#     url = f'{urlApi}/film/{str(titre_id)}'
-#     response = requests.get(url)
-  
-#     if response.status_code == 200:
-#         film = response.json()
-#         return render_template('film.html', film=film['film'])
-#     else:
-#         return jsonify({'error': 'page not found'}), 404
-
-
-
-# # Update a specific film
-# @app.route('/update_film/<int:film_id>', methods=[ 'GET','POST'])
-# @login_required
-# def update_film(film_id):
-#     if request.method == 'GET':
-#         url_get = f'{urlApi}/film/{str(film_id)}'
-#         response_get = requests.get(url_get)
-        
-#         if response_get.status_code == 200:
-#             film = response_get.json()
-#             return render_template('updatefilm.html', film=film['film'])
-#         else:
-#             return jsonify({'error': 'Film not found'}), 404
-
-#     elif request.method == 'POST':
-#         titre = request.form['titre']
-#         genre = request.form['genre']
-#         annee = request.form['annee']
-#         realisateur = request.form['realisateur']
-#         affiche = request.form['affiche']
-#         data = {'titre': titre, 'genre': genre, 'annee': annee, 'realisateur': realisateur, 'affiche': affiche}
-#         url_post = f'{urlApi}/film/{str(film_id)}'
-#         headers = {"Content-Type": "application/json"}
-#         response_post = requests.put(url_post, json=data, headers=headers)
-
-#         if response_post.status_code == 200:
-#             return redirect(url_for('get_userfilms'))
-#         else:
-#             return jsonify({'error': 'Film update failed'}), 500
-#     return redirect(url_for('get_userfilms'))
-
-
-# # Delete a specific video6
-# @app.route('/filmAll/<int:film_id>', methods=['POST', 'DELETE'])
-# def delete_film(film_id):
-#     url = f'{urlApi}/film/{str(film_id)}'
-#     headers = {"Content-Type": "application/json"}
-#     response = requests.delete(url, json=film_id,headers=headers)
-
-#     if response.status_code == 200:
-#         return redirect(url_for('get_films'))
-#     else:
-#         return jsonify({'error': 'page not found'}), 404
-
-
-
-
-#****************  USER FILMS  ***********************************************
-
-
 # Read all user's film
 @app.route("/dashboard", methods=['GET'])
 @login_required
@@ -236,9 +113,6 @@ def get_userinfo():
         url = f'{urlApi}/userinfo/{g.user}'
         response = requests.get(url)
         
-
-
-
         if response.status_code == 200:
         
             data = response.json()
@@ -246,86 +120,6 @@ def get_userinfo():
             # return render_template('info.html', films=films['films'])
         else:
             return jsonify({'error': 'page not found'}), 404
-
-
-
-
-
-
-
-
-# # Read a specific user film
-# @app.route('/film/<int:titre_id>', methods=['GET'])
-# @login_required
-# def get_userfilm(titre_id):
-#     url = f'{urlApi}/{g.user}/film/{str(titre_id)}'
-#     response = requests.get(url)
-
-#     if response.status_code == 200:
-#         film = response.json()
-#         return render_template('film.html', film=film['films'][0], user_id=g.user)
-#     else:
-#         return jsonify({'error': 'page not found'}), 404
-
-
-# @app.route('/addfilm', methods=['GET', 'POST'])
-# @login_required
-# def create_userfilm():
-#     if request.method == 'POST':
-#         id = request.form['film']
-#         data = {'id': id}
-#         url = f'{urlApi}/{g.user}/addfilm'
-#         headers = {"Content-Type": "application/json"}
-#         response = requests.post(url, json=data,headers=headers)
-
-#         if response.status_code == 201:
-#             return render_template('addfilm.html', message='Film ajouté'),201
-#         elif response.status_code == 200:
-#             return render_template('addfilm.html', message='Film ajouté'),200
-#         elif response.status_code == 500:
-#             return render_template('addfilm.html', message='Film déja ajouté'),500
-#         else:
-#             return jsonify({'error': 'page not found'}), 404
-#     else:
-#         return render_template('addfilm.html')
-
-
-# # Delete USER film
-# @app.route('/film/<int:film_id>', methods=['POST', 'DELETE'])
-# @login_required
-# def delete_userfilm(film_id):
-#     url = f'{urlApi}/{g.user}/film/{str(film_id)}'
-#     headers = {"Content-Type": "application/json"}
-#     response = requests.delete(url, json=film_id,headers=headers)
-
-#     if response.status_code == 200:
-#         return redirect(url_for('get_userfilms'))
-#     else:
-#         return jsonify({'error': 'page not found'}), 404
-
-
-
-######################################################################   SEARCH   #############
-
-# @app.route('/search', methods=['GET', 'POST'])
-# @login_required
-# def search_film():
-#     if request.method == 'POST':
-#         titre = request.form['titre']
-#         data = {'titre': titre}
-#         url = f'{urlApi}/search'
-      
-#         headers = {"Content-Type": "application/json"}
-#         response = requests.post(url, json= data, headers=headers)
-
-#         if response.status_code == 200:
-#             results = response.json()
-#             return render_template('search.html', data = results['data'])
-            
-#         else:
-#             return jsonify({'error': 'page not found'}), 404
-         
-#     return redirect(url_for('get_userfilms'))
 
 
 
