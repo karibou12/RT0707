8906f9e2370a235779ab46bfafcdde3d267063cb
guillaume D <karibou12@msn.com>
2024-01-27 22:50:11 +0000

    cleanning code & change scanner code Method

--- Controle_Access_Titres/scanner-qrcode/scanner-qrcode.py
+++ Controle_Access_Titres/scanner-qrcode/scanner-qrcode.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template, request, jsonify
-from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired
-from werkzeug.utils import secure_filename
+from flask import Flask, render_template, redirect, url_for, request, jsonify
 from pyzbar.pyzbar import decode
 from PIL import Image
 import paho.mqtt.client as mqtt
 import os
 from flask import Flask, render_template, send_from_directory
 
-
+#QR folerd
 docker_folder = os.path.abspath(os.getcwd())
 app_folder = os.path.join(docker_folder, 'data')
 
@@ -17,9 +14,6 @@ app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['SECRET_KEY'] = 'your_secret_key'
-
-
-
 
 # MQTT settings
 mqtt_broker = "172.20.0.10"
@@ -43,103 +37,37 @@ mqtt_client.connect(mqtt_broker, mqtt_port, 60)
 # Start the MQTT client loop in a separate thread
 mqtt_client.loop_start()
 
-
-
-
-
-
-
-
 #specify img path
 @app.route('/images/<filename>')
 def get_image(filename):
     images_folder = '/app/data/'
     return send_from_directory(images_folder, filename)
 
-
-class QRCodeForm(FlaskForm):
-    qrcode_image = FileField(validators=[FileRequired()])
-
+#QR reader
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    form = QRCodeForm()
-
-    if form.validate_on_submit():
-        # Sauvegarde du fichier téléchargé
-        qrcode_image = form.qrcode_image.data
-        filename = secure_filename(qrcode_image.filename)
-        filepath = app.config['UPLOAD_FOLDER'] + '/' + filename
-        qrcode_image.save(filepath)
-
-        # Lecture du QR code
-        decoded_objects = decode(Image.open(filepath))
-        data = [obj.data.decode("utf-8") for obj in decoded_objects]
-
-        return render_template('result.html', data=data)
-
-    return render_template('index.html', form=form)
-
-
-@app.route('/qr', methods=['GET', 'POST'])
 def qr():
-    # form = QRCodeForm()
     files = os.listdir(app_folder)
-
-      
-
+   
     return render_template('lecteur.html', files = files)
 
 
-
-
+#QR decoder
 @app.route('/decodeQr/<qr>', methods=['GET','POST'])
 def decode_qr(qr):
-    qr = decode(Image.open(f'{app_folder}/{qr}'))
-    # print(img)
-    # print("----")
-    # print("data {}, type {}".format(img[0].data,type(img[0].data)))
-    # print("------")
-    # print("data décodée {}, type {}".format(img[0].data.decode(), type(img[0].data.decode())))
-    id =qr[0].data.decode()
+    qrd = decode(Image.open(f'{app_folder}/{qr}'))
+    id =qrd[0].data.decode()
     
-    # id = json.dumps(id)
     data = id.split(',')
     data = data[0][8:]
-
-    
+      
+    #send to Mosquitto broker
     mqtt_client.publish(mqtt_topic, data)
 
-    
-    return render_template('qr.html', id= id, data = data)
+    #remove QR
+    os.remove(f'{app_folder}/{qr}')
 
-
-
-
-
-
-
-
-
-
-
-@app.route('/send_message', methods=['POST'])
-def send_message():
-    if request.method == 'POST':
-        message = request.form['message']
-        mqtt_client.publish(mqtt_topic, message)
-        return "Message sent successfully!"
-
-
-
-
-
-
-
-
-
-
-
-
+         
+    return redirect(url_for('qr'))
 
 
 if __name__ == '__main__':
--- Controle_Access_Titres/srv-controle/srv-controle.py
+++ Controle_Access_Titres/srv-controle/srv-controle.py
@@ -2,14 +2,13 @@ from flask import Flask, jsonify
 import paho.mqtt.client as mqtt
 import pymongo
 from bson import ObjectId
-
+import os
+import requests
 
 #***************************     mongodb       **************************
 
 myclient = pymongo.MongoClient('mongodb://172.20.0.1', 27017, username="root", password='1toto;2')
 mydb = myclient["Billetterie"]
-
-     
 
 app = Flask(__name__)
 
@@ -18,33 +17,34 @@ MQTT_BROKER = "172.20.0.10"
 MQTT_PORT = 1883
 MQTT_TOPIC = "QR/topic"
 
+
+#QR folerd
+docker_folder = os.path.abspath(os.getcwd())
+app_folder = os.path.join(docker_folder, 'data')
+
 # Callback when message is received
 def on_message(client, userdata, message):
     payload = message.payload.decode("utf-8")
     # print(f"Received message: {payload}")
     billetId = payload
     print(f' recu de mqtt ##{billetId}##')
-
-
-            
+           
     mycol = mydb["billets"]
     dbrequest = {"_id": ObjectId(str(billetId))}
     liste = mycol.find(dbrequest)
 
     for x in liste:
-        valid = x['isValid']
-        print(f'Valid de la boucle')
+        isValid = x['isValid']
 
-    if valid == 'true':
+    if str(isValid) == 'true':
         print(f'le billet est utilisé. il ne sera plus valide')
         requete = {"_id": ObjectId(str(billetId))}
-        print('proutt')
         modif = { "$set" : {"isValid":'false'}}
         mycol.update_one(requete,modif)
+     
+
     else:
         print('le billet est déja utilisé')
-
-
 
 
 # Connect to MQTT broker and subscribe to topic
@@ -53,6 +53,8 @@ mqtt_client.on_message = on_message
 mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
 mqtt_client.subscribe(MQTT_TOPIC)
 mqtt_client.loop_start()
+
+
 
 @app.route('/')
 def index():
